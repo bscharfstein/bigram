a190ee4e4b64d87d80e73cfc3b6f0e1a64b42fb8
Evan Brown <evanbrown88@gmail.com>
2014-04-30 22:18:38 -0400

    working on decrypt function

--- decrypt.java
+++ decrypt.java
@@ -44,13 +44,15 @@ public class Decrypt {
     }
     
     public static String decrypt(String line) {
+    	System.out.println("line: " + line);
     	/*initialize the emission probabilities*/
     	initEmits(line);
-    
+    	//printEProbs();
     	/* Run the algorithm to build the decrypt array */
     	int maxk = 50;
 		for (int k = 0; k < maxk; k++) {
 			for (int i = 0; i < 27; i++) {
+				decryptArr[i] = 0;
 				for(int j = 0; j<27; j++) {
 					counts[i][j] = 0;
 				}
@@ -96,13 +98,14 @@ public class Decrypt {
 			}
 			//System.out.println(k + ":\t" + totalprob);
 		}
+		printEProbs();
 		
 		/*Decrypt the string using the array, building a string to return*/
 		String decryptedLine = "";
 		for(int i = 0; i < line.length(); i++) {
 			decryptedLine += deconvert((char) decryptArr[(int) convert(line.charAt(i))]);
 		}
-		System.out.println(decryptedLine);
+		System.out.println("DecryptedLine: " + decryptedLine);
 		return decryptedLine;
     }
         
